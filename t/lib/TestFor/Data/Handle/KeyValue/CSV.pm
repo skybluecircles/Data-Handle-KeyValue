e801cb50363ac76723a454814f449b2417b87b25
William Stevenson <wstevenson@maxmind.com>
2014-02-18 09:12:23 -0500

    D::H::KV::CSV and tests

---
+++ t/lib/TestFor/Data/Handle/KeyValue/CSV.pm
@@ -0,0 +1,34 @@
+package TestFor::Data::Handle::KeyValue::CSV;
+
+use Data::Handle::KeyValue::CSV;
+use Test::Class::Moose;
+
+with 'TestRole::Data::Handle';
+
+sub test_csv_with_header {
+    my $self = shift;
+
+    my $csv    = 't/test-data/travel-log.with-header.csv';
+    my $header = 1;
+
+    my $data_handle
+        = Data::Handle::KeyValue::CSV->new( file => $csv, header => $header );
+
+    $self->_test_data_handle( $data_handle, 'csv with header' );
+}
+
+sub test_csv_without_header {
+    my $self = shift;
+
+    my $csv = 't/test-data/travel-log.without-header.csv';
+    my $column_names = [ 'date', 'distance' ];
+
+    my $data_handle = Data::Handle::KeyValue::CSV->new(
+        file         => $csv,
+        column_names => $column_names
+    );
+
+    $self->_test_data_handle( $data_handle, 'csv without header' );
+}
+
+1;
