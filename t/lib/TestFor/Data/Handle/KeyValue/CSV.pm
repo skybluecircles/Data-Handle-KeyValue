0f6d3cb04be98886bda2ec66b4ccb9a5c80a1ba6
William Stevenson <wstevenson@maxmind.com>
2014-02-19 21:00:07 -0500

    Changed attribute name, updated docs and tests

--- t/lib/TestFor/Data/Handle/KeyValue/CSV.pm
+++ t/lib/TestFor/Data/Handle/KeyValue/CSV.pm
@@ -8,11 +8,13 @@ with 'TestRole::Data::Handle';
 sub test_csv_with_header {
     my $self = shift;
 
-    my $csv    = 't/test-data/travel-log.with-header.csv';
-    my $header = 1;
+    my $path_to_csv = 't/test-data/travel-log.with-header.csv';
+    my $header      = 1;
 
-    my $data_handle
-        = Data::Handle::KeyValue::CSV->new( file => $csv, header => $header );
+    my $data_handle = Data::Handle::KeyValue::CSV->new(
+        path_to_csv => $path_to_csv,
+        header      => $header
+    );
 
     $self->_test_data_handle( $data_handle, 'csv with header' );
 }
@@ -20,11 +22,11 @@ sub test_csv_with_header {
 sub test_csv_without_header {
     my $self = shift;
 
-    my $csv = 't/test-data/travel-log.without-header.csv';
+    my $path_to_csv = 't/test-data/travel-log.without-header.csv';
     my $column_names = [ 'date', 'distance' ];
 
     my $data_handle = Data::Handle::KeyValue::CSV->new(
-        file         => $csv,
+        path_to_csv  => $path_to_csv,
         column_names => $column_names
     );
 
