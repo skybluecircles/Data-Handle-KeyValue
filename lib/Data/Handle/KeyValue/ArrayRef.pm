75cbabb679ecc1681ca53306b7a8936bc59af76b
William Stevenson <skyblue@skybluecircles.com>
2014-02-19 20:47:56 -0500

    Documentation for ArrayRef and CSV

--- lib/Data/Handle/KeyValue/ArrayRef.pm
+++ lib/Data/Handle/KeyValue/ArrayRef.pm
@@ -22,3 +22,34 @@ with 'Data::Handle::Role::KeyValue';
 __PACKAGE__->meta()->make_immutable();
 
 1;
+
+=pod
+
+=head1 SYNOPSIS
+
+  use Data::Handle::KeyValue::ArrayRef;
+
+  my $array_ref = [ ... ];
+  my $data_handle = Data::Handle::KeyValue::ArrayRef->new( array_ref => $array_ref );
+
+  while ( my $row = $data_handle->next_row() ) {
+      ...
+  }
+
+=head1 DESCRIPTION
+
+The modules in Data::Handle::KeyValue provide a consistent interface for fetching rows of hashrefs. In particular Data::Handle::KeyValue::ArrayRef does so for an arrayref which already exists in Perl.
+
+=head1 CONSTRUCTOR
+
+=head2 new
+
+Takes a single required attribute called array_ref which must be a reference to an array.
+
+=head1 METHODS
+
+=head2 next_row
+
+Fetches the next element from the array ref which was passed at construction. This element is expected to be a hash ref.
+
+=cut
